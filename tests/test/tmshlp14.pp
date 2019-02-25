649f1022dd61574e9d79411bafff9cd261768bfc
Ryan Joseph <genericptr@gmail.com>
2019-02-25 10:47:06 -0500

    added 2 more tests

---
+++ tests/test/tmshlp14.pp
@@ -0,0 +1,19 @@
+{$mode objfpc}
+{$modeswitch multihelpers}
+
+program tmshlp14;
+
+type
+	THelper1 = class helper for TObject
+		type TInteger = integer;
+	end;
+	THelper2 = class helper for TObject
+		type TString = string;
+	end;
+
+var
+	obj: TObject;
+begin
+	writeln(sizeof(TObject.TInteger));
+	writeln(sizeof(TObject.TString));
+end.
